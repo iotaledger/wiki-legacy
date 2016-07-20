706a071db4d3dec0eb2a25a713ca80493b0b61c7
domschiener <dom@fileyy.com>
2016-07-20 21:54:29 +0200

    Improved byteToTrytes and vice versa

--- byteToTrytes.js
+++ byteToTrytes.js
@@ -1,18 +1,86 @@
-
-
-function byteToTrytes(char) {
+//
+//  Conversion of bytes to trytes.
+//  Input is a string (can be stringified JSON object), return value is Trytes
+//
+//  How the conversion works:
+//    2 Trytes === 1 Byte
+//    There are a total of 27 different tryte values: 9ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//
+//    1. We get the decimal value of an individual ASCII character
+//    2. From the decimal value, we then derive the two tryte values by basically calculating the tryte equivalent (e.g. 100 === 19 + 3 * 27)
+//      a. The first tryte value is the decimal value modulo 27 (27 trytes)
+//      b. The second value is the remainder (decimal value - first value), divided by 27
+//    3. The two values returned from Step 2. are then input as indices into the available values list ('9ABCDEFGHIJKLMNOPQRSTUVWXYZ') to get the correct tryte value
+//
+//   EXAMPLES
+//      Lets say we want to convert the ASCII character "Z".
+//        1. 'Z' has a decimal value of 90.
+//        2. 90 can be represented as 9 + 3 * 27. To make it simpler:
+//           a. First value: 90 modulo 27 is 9. This is now our first value
+//           b. Second value: (90 - 9) / 27 is 3. This is our second value.
+//        3. Our two values are now 9 and 3. To get the tryte value now we simply insert it as indices into '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//           a. The first tryte value is '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'[9] === "I"
+//           b. The second tryte value is '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'[3] === "C"
+//        Our tryte pair is "IC"
+//
+//      RESULT:
+//        The ASCII char "Z" is represented as "IC" in trytes.
+//
+function byteToTrytes(inputString) {
   var availValues = "9ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var trytes = "";
 
-  var asciiValue = char.charCodeAt(0);
+  for (var i = 0; i < inputString.length; i++) {
+    var char = inputString[i];
+    var asciiValue = char.charCodeAt(0);
 
-  var firstValue = asciiValue % 27;
-  var secondValue = (asciiValue - firstValue) / 27;
+    // If not recognizable ASCII character, replace with space
+    if (asciiValue > 255) {
+      asciiValue = 32
+    }
 
-  console.log(firstValue, secondValue);
+    var firstValue = asciiValue % 27;
+    var secondValue = (asciiValue - firstValue) / 27;
 
-  var trytesValue = availValues[firstValue] + availValues[secondValue];
+    var trytesValue = availValues[firstValue] + availValues[secondValue];
 
-  return trytesValue;
+    trytes += trytesValue;
+
+    console.log("Converted char: %s, into trytes %s", char, trytesValue);
+  }
+  console.log("Final Tryte Value:", trytes);
+  return trytes;
 }
 
-console.log(byteToTrytes())
+
+//
+//  Trytes to byte
+//  Reverse operation from the byteToTrytes function in send.js
+//  2 Trytes == 1 Byte
+//  We assume that the trytes are a JSON encoded object thus for our encoding:
+//    First character = {
+//    Last character = }
+//    Everything after that is 9's padding
+//
+function trytesToByte(inputTrytes) {
+  var availValues = "9ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var outputString = "";
+
+  for (var i = 0; i < inputTrytes.length; i += 2) {
+    // get a trytes pair
+    var trytes = inputTrytes[i] + inputTrytes[i + 1];
+
+    var firstValue = availValues.indexOf(trytes[0]);
+    var secondValue = availValues.indexOf(trytes[1]);
+
+    var decimalValue = firstValue + secondValue * 27;
+
+    var character = String.fromCharCode(decimalValue);
+
+    outputString += character;
+
+    console.log("Converted trytes: %s, into char %s", trytes, character);
+  }
+  console.log("Final String:", outputString);
+  return outputString;
+}
